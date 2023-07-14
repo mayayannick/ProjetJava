559594a028cffc446cb78a47800d32e20fd2a23b
bens16 <bloumvoktoin@gmail.com>
2023-07-14 08:51:39 +0200

    Update Formulaire.java
    Généralisation des bibliothèques
    
    Co-Authored-By: Komhidi Jean Jacques <138132937+jean-jacques-komhidi@users.noreply.github.com>
    Co-Authored-By: mayayannick <139135277+mayayannick@users.noreply.github.com>
    Co-Authored-By: nesgui <139131527+nesgui@users.noreply.github.com>

--- Formulaire.java
+++ Formulaire.java
@@ -1,22 +1,7 @@
 package Formulaire;
 
 import java.io.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
